aae8b4a15a4febe27ee528ff1ed3baa1b0095a11
ndng1410 <ndng1410@gmail.com>
2023-02-06 19:29:58 +0700

    re-commit

--- js/filterbtn.js
+++ js/filterbtn.js
@@ -3,8 +3,16 @@ $(document).ready(function(){
     $(this).parent().toggleClass("box-active");
   });
 
-  $(".btn-filter").click(function(){
-    $(".rank-box-item-box li").removeClass("box-active");
-  });
+  // $(".btn-filter").click(function(){
+  //   $(".rank-box-item-box li").removeClass("box-active");
+  // });
 });
+
+function typeListClick(el) {
+  if($(el).hasClass('active--type-list')){}
+  else {
+    $(el).addClass('active--type-list');
+    $('.type-list a').not($(el)).removeClass("active--type-list");
+  }
+};
 
